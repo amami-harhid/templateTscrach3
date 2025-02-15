4298818b01d846ef0d38f29d1f06cb829bc69a55
amami-harhid <amami-thida100@outlook.jp>
2025-02-15 22:29:24 +0900

    update

--- test/test01/build/index.js
+++ test/test01/build/index.js
@@ -1,14 +1,43 @@
-import { Sample } from './sample.js';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import 'https://amami-harhid.github.io/scratch3LikeJsLib/build/likeScratchLib.js';
 // @ts-ignore
-const P = likeScratchLib.process;
-console.log(P);
-/** サンプルインスタンス */
-const sample = new Sample();
-/** メッセージ */
-const message = sample.hello("TsScript");
-console.log(message);
-//sample.method2 = (arg:boolean)=>{return "a"};
-if (sample.method2)
-    console.log(sample.method2(true));
+const Lib = likeScratchLib;
+(function (M, S) {
+    M.preload = function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.loadImage('../assets/Jurassic.svg', 'Jurassic');
+            this.loadSound('../assets/Chill.wav', 'Chill');
+        });
+    };
+    M.prepare = function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            S.stage = new M.Stage();
+            S.stage.addImage(M.images.Jurassic);
+        });
+    };
+    M.setting = function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            // すぐに実行する。
+            S.stage.whenRightNow(function (stage) {
+                // ここでの『this』は Proxy(stage)である。
+                stage.addSound(M.sounds.Chill, { 'volume': 100 });
+            });
+            S.stage.whenFlag(function (stage) {
+                // 「終わるまで音を鳴らす」をずっと繰り返す
+                stage.while(true, (stage) => __awaiter(this, void 0, void 0, function* () {
+                    // 処理が終わるまで待つために await をつける
+                    yield stage.startSoundUntilDone();
+                }));
+            });
+        });
+    };
+})(Lib.Main, Lib.Space);
 //# sourceMappingURL=index.js.map
--- test/test01/index.ts
+++ test/test01/index.ts
@@ -1,13 +1,29 @@
-import {Sample, SampleInt} from './sample.js'
 import 'https://amami-harhid.github.io/scratch3LikeJsLib/build/likeScratchLib.js';
 // @ts-ignore
-const P = likeScratchLib.process;
+const Lib:LikeScratchLib = likeScratchLib;
+(function(M:Process, S:any){
 
-/** サンプルインスタンス */
-const sample:SampleInt = new Sample();
-/** メッセージ */
-const message: string = sample.hello("TsScript");
-console.log(message)
-//sample.method2 = (arg:boolean)=>{return "a"};
-if(sample.method2)
-    console.log(sample.method2(true));
+    M.preload = async function() {
+        this.loadImage('../assets/Jurassic.svg','Jurassic');
+        this.loadSound('../assets/Chill.wav','Chill');
+    }
+    M.prepare = async function() {
+        S.stage = new M.Stage();
+        S.stage.addImage( M.images.Jurassic );
+    }
+    M.setting = async function() {
+        // すぐに実行する。
+        S.stage.whenRightNow( function(stage:Entity){
+            // ここでの『this』は Proxy(stage)である。
+            stage.addSound( M.sounds.Chill, { 'volume' : 100 } );
+        });
+        S.stage.whenFlag( function(stage: Entity){ 
+            // 「終わるまで音を鳴らす」をずっと繰り返す
+            stage.while(true, async (stage: Entity)=>{
+                // 処理が終わるまで待つために await をつける
+                await stage.startSoundUntilDone();
+            });
+        });
+    };
+
+})(Lib.Main, Lib.Space);
