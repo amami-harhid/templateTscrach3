c914ea92a7279ff9b2838a9a5b984be9a1c9820f
amami-harhid <amami-thida100@outlook.jp>
2025-02-24 13:04:07 +0900

    update

--- src/types/scratchjs/s3ControlFunctions/index.d.ts
+++ src/types/scratchjs/s3ControlFunctions/index.d.ts
@@ -1,18 +1,18 @@
 /** 制御処理 */
 export interface S3ControlFunctions {
-    /** 指定したms秒待つ */
+    /** 指定したms秒待つ(await必要) */
     wait(ms: number) : Promise<any>;
-    /** 指定した条件が成立するまで待つ */
+    /** 指定した条件が成立するまで待つ(await任意) */
     waitUntil(condition: Function) : Promise<any>;
-    /** 指定した条件が成立する間、待つ */
+    /** 指定した条件が成立する間、待つ(await任意) */
     waitWhile(condition: Function) : Promise<any>;
-    /** ずっと繰り返す */
+    /** ずっと繰り返す(await任意) */
     forever(process: Function) : Promise<any>;
-    /** 指定した条件が成立する間、繰り返す */
+    /** 指定した条件が成立する間、繰り返す(await任意) */
     while(condition: Function|boolean, process:Function ) : Promise<any>;
-    /** 指定した回数、繰り返す */
+    /** 指定した回数、繰り返す(await任意) */
     repeat(count: number, process:Function ) : Promise<any>;
-    /** 指定した条件が成立するまで、繰り返す */
+    /** 指定した条件が成立するまで、繰り返す(await任意) */
     repeatUntil(condition: Function, process:Function ) : Promise<any>;
     /** すべてを停止する */
     stopAll(): void;
