2097b0d40df2d697d5222b0764be1a50b42d894a
amami-harhid <amami-thida100@outlook.jp>
2025-03-23 14:48:22 +0900

    update

---
+++ src/sample27/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const currentDir = __dirname;
+const simpleDirName = currentDir.replace(/\\/g,'/').replace(/^.+\//, '');
+module.exports = {
+    extends: require.resolve('../../base-webpack.config.js'),
+    output: {
+        path: path.resolve(__dirname, "../../build/"+simpleDirName),
+        filename: './bundle.js',
+    },
+}
